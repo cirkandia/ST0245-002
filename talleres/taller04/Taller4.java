44e0779bbb84cacfaa6d97e2f893060ed84d5589
johan <36931159+cirkandia@users.noreply.github.com>
2021-02-17 08:31:33 -0500

    Add files via upload

---
+++ talleres/taller04/Taller4.java
@@ -0,0 +1,33 @@
+
+class Taller4{
+ 
+    public static int arrayMax(int[] array, int n) {
+        int max, temp;
+        max = array[n];
+        if(n != 0) {
+            temp = arrayMax(array, n-1); 
+            if(temp >= max)
+                max = temp;
+            }
+            return max;
+    }
+ 
+ 
+ 
+    public static boolean groupSum(int start, int[] nums, int target) {
+        if (start == nums.length) 
+            return target == 0; 
+        else
+            return groupSum(start+1, nums, target) 
+            || groupSum(start+1,nums,target-nums[start]);
+ 
+    }
+ 
+    public static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2); 
+        }
+    }
+}
